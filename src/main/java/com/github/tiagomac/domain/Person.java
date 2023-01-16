497c8c4d323bf55ee681a765749b2345dc643b1d
iltifat <Haris.Iltifat@adesso-insurance-solutions.de>
2023-01-16 16:02:49 +0100

    Changes fur Product

--- src/main/java/com/github/tiagomac/domain/Person.java
+++ src/main/java/com/github/tiagomac/domain/Person.java
@@ -10,14 +10,12 @@ import javax.persistence.Table;
 
 import lombok.Data;
 
-@Table(schema="TEST", name="PERSON")
+@Table(schema="MIGSUITE", name="PERSON")
 @Entity
 @Data
 public class Person {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SG_SEQ_PERSON_ID")
-	@SequenceGenerator(name = "SG_SEQ_PERSON_ID", sequenceName = "TEST.SEQ_PERSON_ID")
 	private Long id;
 	
 	@Column
